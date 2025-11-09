c944fddc831db7cc02f006916d5c15e107579b1e
Abhirup Dutta <abhirupdutta@Abhirups-MacBook-Pro.local>
2025-11-08 23:13:08 -0500

    Number of cycles done

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 - Pomodoro timer allows you to have 25 minutes of focus time followed by 5 minutes of break time.
 - Automatic Focus Time - Break Time Cycle
-- Start, Pause & Reset Timer.
+- Start, Pause & Reset Timer
+- Number of Cycles done
 - Autumn-themed Style Sheet
 
-![Screenshot of the pomodoro timer with the clock showing reminaining time, start, pause, and reset buttons, and status area showing focus time or break time](images/pomodoro_screenshot.png)
+![Screenshot of the pomodoro timer with the clock showing reminaining time, start, pause, and reset buttons, and status area showing focus time or break time, and number of cycles done](images/pomodoro_screenshot_v2.png)
--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const FOCUS_MINUTES = 25;
 const BREAK_MINUTES = 5;
 const FOCUS_STATUS = "Focus Time";
 const BREAK_STATUS = "Break Time";
+const CYCLES_DONE = "Cycles Done";
 const focusDuration = FOCUS_MINUTES * 60;
 const breakDuration = BREAK_MINUTES * 60;
 const clockDOMElement = document.getElementById("timerClock");
 const statusDOMElement = document.getElementById("timerStatus");
+const cycleDOMElement = document.getElementById("cycleCount");
 const startButton = document.getElementById("startBtn");
 const pauseButton = document.getElementById("pauseBtn");
 const resetButton = document.getElementById("resetBtn");
@@ -13,6 +15,7 @@ const resetButton = document.getElementById("resetBtn");
 let timer;
 let timeLeft = focusDuration;
 let isFocusTime = true;
+let cycleCount = 0;
 
 function renderTimeForDisplay(time) {
     let displayMinutes = String(Math.floor(time / 60)).padStart(2, "0");
@@ -28,18 +31,35 @@ function updateStatusDisplay(status) {
     statusDOMElement.textContent = status ? FOCUS_STATUS : BREAK_STATUS;
 }
 
-function toggleStatus_And_TimeLeft() {
-    isFocusTime = !isFocusTime;
-    timeLeft = isFocusTime ? focusDuration : breakDuration;
+function updateCyclesDisplay(count) {
+    cycleDOMElement.textContent = `${count} ${CYCLES_DONE}`;
 }
 
-function resetStatus_And_TimeLeft() {
+function toggleStatus_TimeLeft_And_Cycle() {
+    isFocusTime = !isFocusTime;
+    timeLeft = isFocusTime ? focusDuration : breakDuration;
+    if (isFocusTime) {
+        updateCycleCount();
+    }
+}
+
+function resetStatus_TimeLeft_And_Cycle() {
     isFocusTime = true;
     timeLeft = focusDuration;
+    resetCycleCount();
+
 }
 
 function stopTimer() {
     clearInterval(timer);
+}
+
+function updateCycleCount() {
+    cycleCount++;
+}
+
+function resetCycleCount() {
+    cycleCount = 0;
 }
 
 function startTimer() {
@@ -49,9 +69,10 @@ function startTimer() {
             updateClockDisplay(timeLeft);
         } else {
             stopTimer();
-            toggleStatus_And_TimeLeft();
+            toggleStatus_TimeLeft_And_Cycle();
             updateClockDisplay(timeLeft);
             updateStatusDisplay(isFocusTime);
+            updateCyclesDisplay(cycleCount);
             startTimer();
         }
     }, 1000);
@@ -63,9 +84,10 @@ function pauseTimer() {
 
 function resetTimer() {
     stopTimer();
-    resetStatus_And_TimeLeft();
+    resetStatus_TimeLeft_And_Cycle();
     updateClockDisplay(timeLeft);
     updateStatusDisplay(isFocusTime);
+    updateCyclesDisplay(cycleCount);
 }
 
 startButton.addEventListener("click", startTimer);
