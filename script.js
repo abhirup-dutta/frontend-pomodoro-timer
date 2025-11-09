303ab00d5f4b31f10490f80771d52f66f44656b8
Abhirup Dutta <abhirupdutta@Abhirups-MacBook-Pro.local>
2025-11-08 22:17:30 -0500

    Focus / Break Cycle

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const workDuration = 25 * 60;
+const breakDuration = 5 * 60;
 const clockDOMElement = document.getElementById("timerClock");
+const statusDOMElement = document.getElementById("timerStatus");
 const startButton = document.getElementById("startBtn");
 const pauseButton = document.getElementById("pauseBtn");
 const resetButton = document.getElementById("resetBtn");
 
 let timer;
 let timeLeft = workDuration;
+let isFocusTime = true;
 
 function renderTimeForDisplay(time) {
     let displayMinutes = String(Math.floor(time / 60)).padStart(2, "0");
@@ -13,17 +16,30 @@ function renderTimeForDisplay(time) {
     return `${displayMinutes}:${displaySeconds}`;
 }
 
-function updateClockDisplay(displayDOMElement, time) {
-    displayDOMElement.textContent = renderTimeForDisplay(time);
+function updateClockDisplay(time) {
+    clockDOMElement.textContent = renderTimeForDisplay(time);
 }
 
-function startTimerWithParameters(displayDOMElement, updateDisplayFunction) {
+function updateStatusDisplay(status) {
+    statusDOMElement.textContent = status ? "Focus Time" : "Break Time";
+}
+
+function toggleStatus_And_TimeLeft() {
+    isFocusTime = !isFocusTime;
+    timeLeft = isFocusTime ? workDuration : breakDuration;
+}
+
+function startTimer() {
     timer = setInterval(() => {
         if (timeLeft > 0) {
             timeLeft--;
-            updateDisplayFunction(displayDOMElement, timeLeft);
+            updateClockDisplay(timeLeft);
         } else {
             clearInterval(timer);
+            toggleStatus_And_TimeLeft();
+            updateClockDisplay(timeLeft);
+            updateStatusDisplay(isFocusTime);
+            startTimer();
         }
     }, 1000);
 }
@@ -32,19 +48,15 @@ function resetTimeLeft() {
     timeLeft = workDuration;
 }
 
-function startTimer() {
-    startTimerWithParameters(clockDOMElement, updateClockDisplay);
-}
-
 function pauseTimer() {
     clearInterval(timer);
-    updateClockDisplay(clockDOMElement, timeLeft);
+    updateClockDisplay(timeLeft);
 }
 
 function resetTimer() {
     clearInterval(timer);
     resetTimeLeft();
-    updateClockDisplay(clockDOMElement, timeLeft);
+    updateClockDisplay(timeLeft);
 }
 
 startButton.addEventListener("click", startTimer);
